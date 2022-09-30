891268e193adbac9950367d9de67981568d90219
gmiak <kayss.g@gmail.com>
2022-09-30 18:02:23 +0200

    Home page: done

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 // components takes precedence over default styles.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Main } from './screens/main';
--- client/src/screens/home.js
+++ client/src/screens/home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../App.css';
 // @flow
 
 /*
@@ -9,19 +8,35 @@ import '../App.css';
 
 function Home() {
 
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-  
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : "Here in Home "+ data}</p>
-      </header>
+    <div className="containtBody2">
+      <div className="container">
+        <div className="row">
+          <div className="col"><img src="assets/me.PNG" alt="avatar" width="300" height="300" /></div>
+          <div className="col-6">
+            <h4>Hi! Welcome to my page for the course Jsramverk!</h4>
+            I am Georges Kayembe. I'm from Gothenburg and i'm study Web programming at BTH.
+            I have some experience with desktop app using Java - JavaFX - Python & C.
+            When it comes to web development, i do stuff like HTML, PHP, SQL, LINUX and BASH.
+            I have a passion for programming and I’m currently learning Flutter & Dart, React & TypeScript.
+          </div>
+          <div className="w-100"></div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <h4>What it's about</h4>
+            This is a very simple Text Editor created using TypeScript.
+            Its purpose is nothing more than to help me practice using TypeScript
+            and React together while getting a better understanding of user-text interactions.
+          </div>
+
+          <div className="col"><img src="assets/logo.png" alt="avatar" width="300" height="300" /></div>
+          <div className="w-100"></div>
+        </div>
+      </div>
     </div>
   );
 }
