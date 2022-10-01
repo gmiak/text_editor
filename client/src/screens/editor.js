40a93286ac27b1847e91419cf813b68f0d2f1922
gmiak <kayss.g@gmail.com>
2022-10-01 23:41:35 +0200

    Added text editor

--- client/src/screens/editor.js
+++ client/src/screens/editor.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import '../App.css';
+import React, { useState } from 'react';
+import { Editor } from 'primereact/editor';
 // @flow
 
 /*
@@ -7,23 +7,31 @@ import '../App.css';
 ** Now can you run npm run flow to ckeck the files for errors. 
 */
 
-function Editor() {
 
-  const [data, setData] = React.useState(null);
+const TextEditor = () => {
+  const [text1, setText1] = useState('<div>Hello World!</div><div>PrimeReact <b>Editor</b> Rocks</div><div><br></div>');
+  const [text2, setText2] = useState('');
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-  
+  const renderHeader = () => {
+      return (
+          <span className="ql-formats">
+              <button className="ql-bold" aria-label="Bold"></button>
+              <button className="ql-italic" aria-label="Italic"></button>
+              <button className="ql-underline" aria-label="Underline"></button>
+          </span>
+      );
+  }
+
+  const header = renderHeader();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : "Here in Editor "+ data}</p>
-      </header>
-    </div>
+      <div>
+        <div className='space'></div>
+          <div className="card">
+              <Editor headerTemplate={header} style={{ height: '320px' }} value={text2} onTextChange={(e) => setText2(e.htmlValue)} />
+          </div>
+      </div>
   );
 }
 
-export default Editor;
+export default TextEditor;
